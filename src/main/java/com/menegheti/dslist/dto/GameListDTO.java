731428a48dead23fb4811329c7da6bab69cf1ee8
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-11-13 23:10:17 -0300

    GET lists

---
+++ src/main/java/com/menegheti/dslist/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.menegheti.dslist.dto;
+
+import com.menegheti.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
