0f9330c20f6fe92048e0d04532661f7941209a83
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-11-13 22:58:49 -0300

    GET game by id

--- src/main/java/com/menegheti/dslist/services/GameService.java
+++ src/main/java/com/menegheti/dslist/services/GameService.java
@@ -4,19 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.menegheti.dslist.dto.GameDTO;
 import com.menegheti.dslist.dto.GameMinDTO;
 import com.menegheti.dslist.entities.Game;
 import com.menegheti.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
+	
 	@Autowired
 	private GameRepository gameRepository;
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
 	}
+	
 }
